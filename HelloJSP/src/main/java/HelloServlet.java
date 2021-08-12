cf0f75a1b556d6462865661f438aa0017c53f9a2
Jaemin Lim <jmeeen@gmail.com>
2021-08-12 13:53:17 +0900

    Post 전송

--- HelloJSP/src/main/java/HelloServlet.java
+++ HelloJSP/src/main/java/HelloServlet.java
@@ -13,17 +13,38 @@ public class HelloServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		// 응답의 페이지 타입 결정
+		resp.setContentType("text/html; charset=UTF-8");
+
+		// 파라미터 확인
 		String name = req.getParameter("name");
 		if (name == null) {
 			name = "Anonymous";
 		}
-		
+
 		// 응답 출력을 위한 Writer
-		
 		PrintWriter out = resp.getWriter();
 		out.println("<h3>Hello Servlet</h3>");
-		out.println("<p>Welcome, "+ name +"</p>");
-		super.doGet(req, resp);
+		out.println("<p>Welcome, " + name + "</p>");
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		// 페이지 응답 설정
+		resp.setContentType("text/html; charset=UTF-8");
+
+		// 폼 데이터 받기(파라미터 받기)
+		// input tag의 name 속성의 이름
+		String firstname = req.getParameter("First_name");
+		String lastname = req.getParameter("Last_name");
+
+		PrintWriter out = resp.getWriter();
+		out.println("</h3>From Data</h3>");
+		out.println("<p>성:" + lastname + "</p>");
+		out.println("<p>이름:" + firstname + "</p>");
+
 	}
 
 }
